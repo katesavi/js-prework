6f3c11b2aa2865d7470ba397a31905ae5e32beca
Piotr Loch <piotr.loch@sartorius.com>
2023-03-03 16:49:47 +0100

    zadanie domowe

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,22 @@
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
-
     document.getElementById('messages').appendChild(div);
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
+}
+
+function getMoveName(move){
+	if( move === 1){
+		return 'kamień';
+	}
+	else if( move === 2){
+		return 'papier';
+	}
+	else if( move === 3){
+		return 'nożyce';
+	}
 }
 
